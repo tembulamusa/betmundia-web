222fc323976c8b959805a99e231b3a7bc0526153
Moses tembula <jameswangabivan@gmail.com>
2026-03-31 15:10:49 +0300

    change odds on promo data

--- src/components/pages/promotions/promoData.js
+++ src/components/pages/promotions/promoData.js
@@ -23,8 +23,8 @@ export const promoData = [
       e.g. Manchester City vs Arsenal
       Bayern vs Bremen
       Manchester United vs Chelsea
-      3.⁠ ⁠Each selection should carry a minimum of 1.80 odds on 1 x 2 markets on soccer games.
-     e.g. 1.Arsenal – 1.89
+      3.⁠ ⁠Each selection should carry a minimum of 2.5 odds on 1 x 2 markets on soccer games.
+     e.g. 1.Arsenal – 2.5
             2.Bayern – 2.01
             1.Manchester United – 2.39
              TOTAL odds 9.03
