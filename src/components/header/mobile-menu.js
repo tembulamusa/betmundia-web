71d0e91e2f02870fd9729f8dfb766729b7f9e05b
Moses tembula <jameswangabivan@gmail.com>
2026-04-07 19:05:10 +0300

    dd

--- src/components/header/mobile-menu.js
+++ src/components/header/mobile-menu.js
@@ -159,11 +159,11 @@ function MobileMenu(props) {
               <IoListCircleOutline className="mr-3 inline-block" />
               My bets
             </Link>
-
+            {/* 
             <Link to="/promotions" className="cd-l">
               <FaGifts className="mr-3 inline-block" />
               Promotions
-            </Link>
+            </Link> */}
 
             <Link to="/exclude" className="cd-l">
               <MdCancel className="mr-3 inline-block" />
