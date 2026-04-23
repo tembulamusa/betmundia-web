4ea1460a77c6b381d643af8caa710f3436843daf
Moses tembula <jameswangabivan@gmail.com>
2026-04-23 20:06:07 +0300

    change the sportrader link

--- src/components/header/big-icon-nav.js
+++ src/components/header/big-icon-nav.js
@@ -36,7 +36,7 @@ const BigIconMenu = () => {
         { name: "sports", icon: "sports.svg", link: '/sports', parentTo: "sportscategories" },
         // {name: "virtuals", icon:"virtuals.svg", link:"/virtuals", parentTo:null},
         { name: "promotions", icon: "promos.svg", link: "/promotions", parentTo: null },
-        { name: "livescore", icon: "livescore.svg", link: "https://statshub.sportradar.com/betmundialsmts/en/sport/1", parentTo: null },
+        { name: "livescore", icon: "livescore.svg", link: "https://statshub.sportradar.com/betmundialsmts/en/sport/1/tournament/17", parentTo: null },
         // {name: "basketball", icon:"basketball.svg", link:"/#basketball", parentTo:null},
         // {name: "cricket", icon:"cricket.svg", link:"/#cricket", parentTo:null},
         // {name: "tennis", icon:"tennis.svg", link:"/#tennis", parentTo:null},       
